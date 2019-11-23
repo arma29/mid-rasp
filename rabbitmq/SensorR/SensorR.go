09f2533bba9a9b8429ab2c07ec73530b6f9ea6d8
Arnaldo Morais <arma@cin.ufpe.br>
2019-11-23 18:35:42 -0300

    Merge fix

--- rabbitmq/SensorR/SensorR.go
+++ rabbitmq/SensorR/SensorR.go
@@ -1,78 +1,79 @@
 package sensorR
 
-// import (
-// 	"github.com/streadway/amqp"
-// 	"encoding/json"
-// 	"fmt"
-// 	"time"
-// 	"os"
-// 	"strconv"
-	
-// 	"github.com/arma29/mid-mid-perf/shared"
-// )
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 
-// func main() {
-// 	// Get Argument from command Line
-// 	if len(os.Args) != 3 {
-// 		fmt.Printf("Missing arguments: %s number\n", os.Args[0])
-// 		os.Exit(1)
-// 	}
+	"github.com/streadway/amqp"
 
-// 	ipContainer := os.Args[1]
+	"github.com/arma29/mid-mid-perf/shared"
+)
 
-// 	// conecta ao servidor de mensageria
-// 	conn, err := amqp.Dial("amqp://guest:guest@" + 
-// 		ipContainer + ":" + 
-// 		strconv.Itoa(shared.RABBITMQ_PORT) +"/")
-// 	shared.CheckError(err)
-// 	defer conn.Close()
+func main() {
+	// Get Argument from command Line
+	if len(os.Args) != 3 {
+		fmt.Printf("Missing arguments: %s number\n", os.Args[0])
+		os.Exit(1)
+	}
 
-// 	// cria o canal
-// 	ch, err := conn.Channel()
-// 	shared.CheckError(err)
-// 	defer ch.Close()
+	ipContainer := os.Args[1]
 
-// 	// declara  filas, cria se não existir
-// 	requestQueue, err := ch.QueueDeclare( // fila de envio
-// 		"request", false, 	false, 	false, 	false, 	nil, )	
-// 	shared.CheckError(err)
+	// conecta ao servidor de mensageria
+	conn, err := amqp.Dial("amqp://guest:guest@" +
+		ipContainer + ":" +
+		strconv.Itoa(shared.RABBITMQ_PORT) + "/")
+	shared.CheckError(err)
+	defer conn.Close()
 
-// 	replyQueue, err := ch.QueueDeclare( // fila de respostas	
-// 		"response", false, false, false, false, nil, )
-// 	shared.CheckError(err)
+	// cria o canal
+	ch, err := conn.Channel()
+	shared.CheckError(err)
+	defer ch.Close()
 
-// 	// cria consumidor <-> fila de respostas -> async
-// 	msgsFromServer, err := ch.Consume(replyQueue.Name, "", true, false,
-// 		false, false, nil, )
-// 	shared.CheckError(err)
+	// declara  filas, cria se não existir
+	requestQueue, err := ch.QueueDeclare( // fila de envio
+		"request", false, false, false, false, nil)
+	shared.CheckError(err)
 
-// 	number, _ := strconv.Atoi(os.Args[2])
-// 	fmt.Println("Fibonacci,Answer,Time")
-// 	for i := 0; i<shared.SAMPLE_SIZE; i++{
+	replyQueue, err := ch.QueueDeclare( // fila de respostas
+		"response", false, false, false, false, nil)
+	shared.CheckError(err)
 
-// 		t1 := time.Now()
+	// cria consumidor <-> fila de respostas -> async
+	msgsFromServer, err := ch.Consume(replyQueue.Name, "", true, false,
+		false, false, nil)
+	shared.CheckError(err)
 
-// 		// prepara request
-// 		msgRequest := shared.Request{Req: int32(number)} // Fibonacci 5
-// 		msgRequestBytes,err := json.Marshal(msgRequest)
-// 		shared.CheckError(err)
+	number, _ := strconv.Atoi(os.Args[2])
+	fmt.Println("Fibonacci,Answer,Time")
+	for i := 0; i < shared.SAMPLE_SIZE; i++ {
 
-// 		// publica request <-> fila de envio 
-// 		err = ch.Publish("", requestQueue.Name, false, false,
-// 			amqp.Publishing{ContentType: "text/plain", Body: msgRequestBytes,})
-// 		shared.CheckError(err)
+		t1 := time.Now()
 
-// 		// recebe resposta em bytes
-// 		x := <- msgsFromServer
+		// prepara request
+		msgRequest := shared.Request{Req: int32(number)} // Fibonacci 5
+		msgRequestBytes, err := json.Marshal(msgRequest)
+		shared.CheckError(err)
 
-// 		// deserializa
-// 		msgReply := shared.Response{}
-// 		err = json.Unmarshal([]byte(x.Body), &msgReply)
-// 		shared.CheckError(err)
+		// publica request <-> fila de envio
+		err = ch.Publish("", requestQueue.Name, false, false,
+			amqp.Publishing{ContentType: "text/plain", Body: msgRequestBytes})
+		shared.CheckError(err)
 
-// 		t2 := time.Now()
-// 		xtime := float64(t2.Sub(t1).Nanoseconds()) / 1000000
-// 		s := fmt.Sprintf("%d,%d,%f", number, msgReply.Res, xtime)
-// 		fmt.Println(s)
-// 	}
-// }
+		// recebe resposta em bytes
+		x := <-msgsFromServer
+
+		// deserializa
+		msgReply := shared.Response{}
+		err = json.Unmarshal([]byte(x.Body), &msgReply)
+		shared.CheckError(err)
+
+		t2 := time.Now()
+		xtime := float64(t2.Sub(t1).Nanoseconds()) / 1000000
+		s := fmt.Sprintf("%d,%d,%f", number, msgReply.Res, xtime)
+		fmt.Println(s)
+	}
+}
