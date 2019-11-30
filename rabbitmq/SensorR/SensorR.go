94aad8e6afa48325106db198d2e0df9294495d93
Arnaldo Rafael Morais Andrade <arma@uhwc04.windows.cin.ufpe.br>
2019-11-30 16:46:27 -0300

    Add GPIO logic

--- rabbitmq/SensorR/SensorR.go
+++ rabbitmq/SensorR/SensorR.go
@@ -12,6 +12,7 @@ import (
 	rad "github.com/arma29/mid-rasp/radiation"
 	queue "github.com/arma29/mid-rasp/sensorqueue"
 	"github.com/arma29/mid-rasp/shared"
+	"github.com/stianeikeland/go-rpio"
 )
 
 var (
@@ -25,6 +26,8 @@ var (
 
 	amqpURI string
 	err     error
+
+	pin rpio.Pin
 )
 
 func main() {
@@ -33,6 +36,9 @@ func main() {
 		fmt.Printf("Missing arguments: %s number\n", os.Args[0])
 		os.Exit(1)
 	}
+
+	// Prepara o GPIO
+	initGPIO() // Pin 18
 
 	user := os.Args[1]
 	password := os.Args[2]
@@ -74,10 +80,26 @@ func main() {
 				go waitMsgs(msgsFromServer)
 			}
 		}
+		// Já deixa o LED apagado
+		pin.Low()
+
 		// Garantir taxa máxima
 		time.Sleep(shared.REAL_TIME)
 	}
 
+}
+
+func initGPIO() {
+	// Prepara o GPIO
+	rpioErr := rpio.Open()
+	if rpioErr != nil {
+		panic(fmt.Sprint("Unable to open gpio",
+			rpioErr.Error()))
+	}
+	defer rpio.Close()
+
+	pin = rpio.Pin(18)
+	pin.Output()
 }
 
 func initRabbit() {
@@ -124,7 +146,9 @@ func waitMsgs(ch <-chan amqp.Delivery) {
 		fmt.Printf("Falha registrada em: ")
 		fmt.Println(time.Unix(0, msgReply.Timestamp))
 
-		// Acender o Led GPIO lib
+		// Acende o LED
+		pin.High()
+
 	}
 }
 
